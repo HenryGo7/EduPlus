1b7bd41690f6a5ce7044aa79a312e51a171e7692
Luis Velasco <luiselsw27@gmail.com>
2026-04-14 00:10:34 +0200

    feat: funcion boton cerrar modal y creacion modal recuperar contraseña

--- edu-plus/src/app/features/contactanos/contactanos.ts
+++ edu-plus/src/app/features/contactanos/contactanos.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-contactanos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contactanos.html',
   styleUrl: './contactanos.css',
 })
-export class Contactanos {}
+export class Contactanos {
+
+formData = {
+    nombre: '',
+    email: '',
+    asunto: '',
+    mensaje: ''
+  };
+
+  enviado = false;
+  cargando = false;
+
+  onSubmit() {
+    if (!this.formData.nombre || !this.formData.email || !this.formData.mensaje) return;
+    this.cargando = true;
+    setTimeout(() => {
+      this.cargando = false;
+      this.enviado = true;
+      this.formData = { nombre: '', email: '', asunto: '', mensaje: '' };
+    }, 1200);
+  }
+
+  resetForm() {
+    this.enviado = false;
+  }
+
+
+}
